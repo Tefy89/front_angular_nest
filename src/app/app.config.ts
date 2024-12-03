9c05fde7cde0b81ab2f2f984402c1cfdcbddbc7f
Tefy89 <eds.ordonez@yavirac.edu.ec>
2024-12-03 18:51:42 -0500

    cambios_front

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { BrowserModule, platformBrowser, provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideRouter(routes), provideHttpClient(withFetch())]
+  //providers: [provideRouter(routes), provideHttpClient(withFetch()), importProvidersFrom(BrowserModule), importProvidersFrom(BrowserAnimationsModule)]
+  //validar si se debe importar provideClientHydration
 };
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: false,
-  
+
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  private authService = inject(AuthService)
 
+  loginForm = new FormGroup({
+    email: new FormControl("", [Validators.email, Validators.required]), // valores que quedarán por defecto
+    password: new FormControl("", Validators.required) // valores que quedarán por defecto 
+  })
+
+  funIngresar() {
+    this.authService.loginConNest(this.loginForm.value).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
